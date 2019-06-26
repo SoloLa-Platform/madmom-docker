1360e321aeef773b54c4237d7c083798ae431c91
YKhorizon <ykhorizon.light@gmail.com>
2019-06-26 21:39:03 +0800

    update stable docker image & script

--- beat_tracking.py
+++ beat_tracking.py
@@ -44,28 +44,30 @@ if __name__ == '__main__':
 
     OUTPUT_PREFIX = "./output/"
 
-    filename = sys.argv[1]
-    filenamePieces = filename.split(".")
+    filePath = sys.argv[1]
+    filePathPieces = os.path.basename(filePath).split(".")
+    print("\n")
+    print(filePathPieces)
 
-    if filenamePieces[1] != 'wav' and  filenamePieces[1] != 'mp3':
+    if filePathPieces[1] != 'wav' and  filePathPieces[1] != 'mp3':
         print("unsupported audio type (only support mp3,wav)")
         exit()
 
-    if filenamePieces[1] == 'mp3' or  filenamePieces[1] == 'MP3':
+    if filePathPieces[1] == 'mp3' or  filePathPieces[1] == 'MP3':
         print("[Info] Automatically convert .mp3 to .wav by ffmpeg")
-        subprocess.run(["ffmpeg", "-i", "{}".format(filename), "{}.wav".format(filenamePieces[0])])
+        # subprocess.run(["ffmpeg", "-i", "{}".format(filePath), "{}.wav".format(filePathPieces[0])])
     
-    outputDir = "{}{}/".format(OUTPUT_PREFIX, filenamePieces[0])
+    outputDir = "{}{}/".format(OUTPUT_PREFIX, filePathPieces[0])
     create_folder(outputDir)
 
     print("Start beat_tracking....")
-    beats = beat_tracking(filename)
+    beats = beat_tracking(filePath)
     write_beats(beats, "{}/beats.txt".format(outputDir))
     print("Finish beat_tracking")
 
     print("Start downbeat_tracking")
-    downbeats = downbeat_tracking(filename)
-    write_downbeats(downbeats, "{}/downbeat.txt".format(outputDir))
+    downbeats = downbeat_tracking(filePath)
+    write_downbeats(downbeats, "{}/downbeats.txt".format(outputDir))
     print("Finish downbeat_tracking")
 
 
--- common_use_case.sh
+++ common_use_case.sh
@@ -1,5 +1,20 @@
-# env: windows git bash
+# env: windows cmd.exe 
 # execution location: solola/madmon_beat_tracking
 # input audio location: locate at same directory with beat_tracking.py, e.g. test.mp3
 # output: output/[filename]/beats.txt &　output/[filename]/downbeats.txt e.g. 　output/test/downbeats.txt 
-docker run -ti --rm -v E:\workplace\projects\solola\solola\madmom_beat_tracking:/madmom madmom:test python beat_tracking.py test.mp3
+
+# docker image: madmom_beat_tracking:prod
+# only mount input & output dir
+docker build -t madmom_beat_tracking:prod -f production.dockerfile .
+
+docker run -ti --rm -v E:\workplace\projects\solola\madmom-docker\input:/madmom-docker/input^
+-v E:\workplace\projects\solola\madmom-docker\output:/madmom-docker/output^
+madmom_beat_tracking:prod python beat_tracking.py input/test.mp3
+
+# docker image: madmom_beat_tracking:dev
+# mount source codes
+docker build -t madmom_beat_tracking:dev -f dev.dockerfile .
+
+docker run -ti --rm -v E:\workplace\projects\solola\madmom-docker:/madmom-docker/^ 
+madmom_beat_tracking:dev python beat_tracking.py input/test.mp3
+
